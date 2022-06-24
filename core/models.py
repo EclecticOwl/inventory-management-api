2eb0d6c872800b80f6541560276625af8183debd
Michael Tillman <mnamllit@gmail.com>
2022-06-24 00:47:42 -0500

    Added a model, serializer, and view for the product class. May change later in order to incorporate categories and/or tags.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=300)
+    price_per_unit = models.FloatField()
+    quantity = models.PositiveIntegerField()
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
